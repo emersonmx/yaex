7c9c08a26b56c79bfe7e3f97ad158b7094f2a5d4
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-07-24 13:48:41 -0300

    Remove str cast from tests

--- tests/yaex_test.py
+++ tests/yaex_test.py
@@ -15,7 +15,7 @@ from yaex import (
 
 def test_should_return_an_empty_string_when_no_commands() -> None:
     buffer = yaex()
-    assert str(buffer) == ""
+    assert buffer == ""
 
 
 def test_should_raise_error_when_insert_into_an_empty_buffer() -> None:
@@ -29,7 +29,7 @@ def test_should_append_a_line_into_an_empty_buffer() -> None:
     buffer = yaex(
         append("Hello World"),
     )
-    assert str(buffer) == "Hello World\n"
+    assert buffer == "Hello World\n"
 
 
 def test_should_raise_error_when_delete_a_line_of_an_empty_buffer() -> None:
@@ -44,14 +44,14 @@ def test_should_insert_a_line_after_an_append() -> None:
         append("Hello"),
         insert("World"),
     )
-    assert str(buffer) == "World\nHello\n"
+    assert buffer == "World\nHello\n"
 
 
 def test_should_read_string() -> None:
     buffer = yaex(
         read_string("Hello World\n"),
     )
-    assert str(buffer) == "Hello World\n"
+    assert buffer == "Hello World\n"
 
 
 def test_should_insert_on_the_penultimate_line_by_default() -> None:
@@ -62,7 +62,7 @@ def test_should_insert_on_the_penultimate_line_by_default() -> None:
         read_string("first line\nsecond line\nthird line\nfifth line\n"),
         insert("fourth line"),
     )
-    assert str(buffer) == expectd_buffer
+    assert buffer == expectd_buffer
 
 
 def test_should_append_on_the_last_line_by_default() -> None:
@@ -70,7 +70,7 @@ def test_should_append_on_the_last_line_by_default() -> None:
         read_string("Hello\n"),
         append("World"),
     )
-    assert str(buffer) == "Hello\nWorld\n"
+    assert buffer == "Hello\nWorld\n"
 
 
 def test_should_delete_last_line_by_default() -> None:
@@ -78,7 +78,7 @@ def test_should_delete_last_line_by_default() -> None:
         read_string("Hello\nWorld\n"),
         delete(),
     )
-    assert str(buffer) == "Hello\n"
+    assert buffer == "Hello\n"
 
 
 def test_should_insert_on_the_first_line() -> None:
@@ -87,7 +87,7 @@ def test_should_insert_on_the_first_line() -> None:
         at_first_line(),
         insert("first line"),
     )
-    assert str(buffer) == "first line\nsecond line\nthird line\n"
+    assert buffer == "first line\nsecond line\nthird line\n"
 
 
 def test_should_append_on_the_second_line() -> None:
@@ -96,7 +96,7 @@ def test_should_append_on_the_second_line() -> None:
         at_first_line(),
         append("second line"),
     )
-    assert str(buffer) == "first line\nsecond line\nthird line\n"
+    assert buffer == "first line\nsecond line\nthird line\n"
 
 
 def test_should_delete_the_first_line() -> None:
@@ -105,7 +105,7 @@ def test_should_delete_the_first_line() -> None:
         at_first_line(),
         delete(),
     )
-    assert str(buffer) == "World\n"
+    assert buffer == "World\n"
 
 
 def test_should_insert_on_the_penultimate_line() -> None:
@@ -114,7 +114,7 @@ def test_should_insert_on_the_penultimate_line() -> None:
         at_last_line(),
         insert("second line"),
     )
-    assert str(buffer) == "first line\nsecond line\nthird line\n"
+    assert buffer == "first line\nsecond line\nthird line\n"
 
 
 def test_should_append_on_the_last_line() -> None:
@@ -123,7 +123,7 @@ def test_should_append_on_the_last_line() -> None:
         at_last_line(),
         append("third line"),
     )
-    assert str(buffer) == "first line\nsecond line\nthird line\n"
+    assert buffer == "first line\nsecond line\nthird line\n"
 
 
 def test_should_delete_last_line() -> None:
@@ -132,7 +132,7 @@ def test_should_delete_last_line() -> None:
         at_last_line(),
         delete(),
     )
-    assert str(buffer) == "first line\nsecond line\n"
+    assert buffer == "first line\nsecond line\n"
 
 
 def test_should_raise_error_when_move_the_cursor_to_an_invalid_line() -> None:
@@ -163,7 +163,7 @@ def test_should_insert_at_any_line() -> None:
         insert("third line"),
     )
     assert (
-        str(buffer)
+        buffer
         == "first line\nsecond line\nthird line\nfourth line\nfifth line\n"
     )
 
@@ -175,7 +175,7 @@ def test_should_append_at_any_line() -> None:
         append("third line"),
     )
     assert (
-        str(buffer)
+        buffer
         == "first line\nsecond line\nthird line\nfourth line\nfifth line\n"
     )
 
@@ -188,4 +188,4 @@ def test_should_delete_any_line() -> None:
         at_line(3),
         delete(),
     )
-    assert str(buffer) == "first line\nsecond line\nfourth line\nfifth line\n"
+    assert buffer == "first line\nsecond line\nfourth line\nfifth line\n"
